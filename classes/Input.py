4494e531b96ea6762a4cd99566cf0e3c1d501ba6
Leo <thcheetah777@gmail.com>
2020-09-24 10:53:26 -0700

    Create checkForInput method

--- classes/Input.py
+++ classes/Input.py
@@ -3,3 +3,7 @@ import pygame
 class Input(object):
     def __init__(self, ent):
         self.ent = ent
+
+    def checkForInput(self):
+        events = pygame.event.get()
+        self.checkForKeyboardInput()
