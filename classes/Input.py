6d3bc290cdcaf0da2fc469208d11fb97888068f7
Leo <thcheetah777@gmail.com>
2020-09-24 10:57:41 -0700

    Create method checkForKeyboardInput

--- classes/Input.py
+++ classes/Input.py
@@ -7,3 +7,13 @@ class Input(object):
     def checkForInput(self):
         events = pygame.event.get()
         self.checkForKeyboardInput()
+
+    def checkForKeyboardInput(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[K_LEFT] or keys[K_a]:
+            self.ent.traits["goTrait"].dir = -1
+        elif keys[K_RIGHT] or keys[K_d]:
+            self.ent.traits["goTriat"].dir = 1
+        else:
+            self.ent.traits["goTriat"].dir = 0
