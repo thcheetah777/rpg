248d874272553b6edf8d54eda57d2f2ddf535935
Leo <thcheetah777@gmail.com>
2020-09-24 11:29:01 -0700

    Create init method

--- entities/Player.py
+++ entities/Player.py
@@ -3,3 +3,8 @@ import sys
 sys.path.append('../')
 from entities.EntityBase import EntityBase
 from classes.Input import Input
+
+class Player(EntityBase):
+    def __init__(self, speed, screen):
+        super(Player, self).__init__()
+        
