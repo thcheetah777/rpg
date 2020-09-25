ba45c2aec8ced2e5e5e529d01cff6e82672d722c
Leo <thcheetah777@gmail.com>
2020-09-25 11:51:54 -0700

    Create sound class

---
+++ classes/Sound.py
@@ -0,0 +1,11 @@
+import pygame
+pygame.mixer.init()
+
+class Sound:
+    def __init__(self):
+        self.musicChannel = pygame.mixer.Channel(0)
+        self.musicChannel.set_volume(0.2)
+        self.soundtrack = pygame.mixer.Sound("./sfx/main.mp3")
+
+    def playMusic(self, music):
+        self.musicChannel.play(music)
