5962065c3f5617ade94afcb4aa0d79cafe98fa7b
Leo <thcheetah777@gmail.com>
2020-09-25 16:27:04 -0700

    Comment entities/EntityBase.py

--- entities/EntityBase.py
+++ entities/EntityBase.py
@@ -1,7 +1,12 @@
+# entities/EntityBase
+# Import pygame
 import pygame
 
+# EntityBase class
 class EntityBase(object):
+    # Initializer
     def __init__(self, x, y):
+        # Properties
         self.type = ""
         self.rect = [x, y]
         self.alive = True
